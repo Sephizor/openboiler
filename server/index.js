aeea797a85864b2bb34959406ba8704bee7cde07
Stephen Bayne <stephen.bayne@ncr.com>
2015-12-17 13:34:31 +0000

    Simplify open, read, close

--- server/index.js
+++ server/index.js
@@ -1,15 +1,16 @@
 var gpio = require('pi-gpio');
 
 setInterval(function () {
-	gpio.open(7, "in", function (err) {
-		gpio.read(7, function (err, value) {
-			if(err) {
-				console.log(err);
-			}
-			else {
-				console.log(value);
-			}
-		});
-	});
-	gpio.close(7);
+    gpio.open(7, "in");
+
+    gpio.read(7, function (err, value) {
+        if(err) {
+            console.log(err);
+        }
+        else {
+            console.log(value);
+        }
+    });
+
+    gpio.close(7);
 }, 5000);
