947f738eb51302b3027acf026f353868ddc7f55e
Stephen Bayne <stephen.bayne@ncr.com>
2015-12-17 13:17:33 +0000

    Close the pin after each use and log either error or value

--- server/index.js
+++ server/index.js
@@ -2,8 +2,14 @@ var gpio = require('pi-gpio');
 
 setInterval(function () {
 	gpio.open(7, "in", function (err) {
-		gpio.read(7, function (data) {
-			console.log(data);
+		gpio.read(7, function (err, value) {
+			if(err) {
+				console.log(err);
+			}
+			else {
+				console.log(value);
+			}
 		});
 	});
+	gpio.close(7);
 }, 5000);
