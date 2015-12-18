d29179ed13442e1122bfa4c01aa1f29a9168bdeb
Stephen Bayne <stephen.bayne@ncr.com>
2015-12-18 15:05:32 +0000

    Start program on boot

--- server/index.js
+++ server/index.js
@@ -7,6 +7,73 @@ var cors = require('cors');
 
 // Web API
 
+var profiles = [];
+var activeProfile = {};
+var activeTemperature = 7;
+
+Date.prototype.getDayOfWeek = function(){   
+    return ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"][ this.getDay() ];
+};
+
+var currentDay = new Date().getDayOfWeek();
+var currentHour = new Date().getHours();
+
+var getProfiles = function () {
+    profiles.length = 0;
+    var profilePaths = fs.readdirSync('./profiles');
+
+    profilePaths.forEach(function (path) {
+        var contents = fs.readFileSync('./profiles/' + path, 'utf-8');
+        profiles.push(JSON.parse(contents));
+    });
+}
+
+getProfiles();
+
+var getActiveProfile = function () {
+    var activeName = fs.readFileSync('./activeprofile', 'utf-8').trim();
+    var filtered = profiles.filter(function (profile) {
+        return profile.name == activeName;
+    })[0];
+
+    return filtered;
+};
+
+var setCurrentHour = function () {
+    currentDay = new Date().getDayOfWeek();
+    currentHour = new Date().getHours();
+    setActiveTemperature();
+};
+
+var setActiveTemperature = function () {
+    var matchedDay = activeProfile.days.filter(function (day) {
+        return "Friday" == currentDay;
+    })[0];
+
+    activeTemperature = matchedDay.times.filter(function (time) {
+        return time.hour == currentHour;
+    })[0].temperature;
+};
+
+activeProfile = getActiveProfile();
+
+var startTiming = function () {
+    setInterval(setCurrentHour(), 3600000);
+};
+
+var sync = function () {
+    var dateToCheck = new Date();
+    dateToCheck.setHours(dateToCheck.getHours() + 1);
+    dateToCheck.setMinutes(0);
+    dateToCheck.setSeconds(0);
+    dateToCheck.setMilliseconds(0);
+    var timeUntilNextHour = dateToCheck.getTime() - Date.now();
+    setTimeout(startTiming(), timeUntilNextHour);
+};
+
+sync();
+setActiveTemperature();
+
 var whitelist = [
     'http://192.168.1.180:8080'
 ];
@@ -19,25 +86,22 @@ var corsOptions = {
 };
 
 app.get('/profiles', cors(corsOptions), function (req, res) {
-    var dir = './profiles';
-    fs.readdir(dir, function (err, files) {
-        var profiles = [];
-        if(!err && files) {
-            files.forEach(function (file) {
-                var contents = fs.readFileSync(dir + '/' + file, 'utf-8');
-                profiles.push(JSON.parse(contents));
-            });
-            res.json(profiles);
-        }
-        else {
-            res.json([]);
-        }
-    });
+    getProfiles();
+    res.json(profiles);
 });
 
 app.options('/profile', cors(corsOptions));
 app.post('/profile', cors(corsOptions), function (req, res) {
 
+});
+
+app.get('/activeProfile', cors(corsOptions), function (req, res) {
+    res.json(activeProfile);
+});
+
+app.options('/activeProfile', cors(corsOptions));
+app.post('/activeProfile', cors(corsOptions), function (req, res) {
+    fs.writeFileSync('./activeprofile', req.body.name);
 });
 
 app.options('/temperature', cors(corsOptions));
@@ -53,6 +117,9 @@ var tempValue = 0;
 var humValue = 0;
 
 wss.on('connection', function (ws) {
+    ws.on('open', function () {
+        console.log('Client connected');
+    })
     ws.on('message', function (message) {
         if(message == 'isOn') {
             ws.send(boilerState);
@@ -64,6 +131,9 @@ wss.on('connection', function (ws) {
                 ws.send(JSON.stringify({ temp: tempValue, humidity: humValue}));
             }, 30000);
         }
+    });
+    ws.on('close', function () {
+        console.log('Client disconnected');
     });
 });
 
