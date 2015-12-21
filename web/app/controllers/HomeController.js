af0d75c643e706c2a3066a08c1abc6f531200147
Stephen Bayne <stephen.bayne@ncr.com>
2015-12-21 13:16:03 +0000

    Attempt to format current date

--- web/app/controllers/HomeController.js
+++ web/app/controllers/HomeController.js
@@ -1,3 +1,7 @@
-angular.module('openboiler').controller('HomeController', ['$scope', function ($scope) {
-    
+angular.module('openboiler').controller('HomeController', ['$scope', '$filter', function ($scope, $filter) {
+	$scope.formatDate = function () {
+		var date = new Date();
+
+		return $filter('date')(date, 'EEEE dd MMMM yyyy');
+	};
 }]);
