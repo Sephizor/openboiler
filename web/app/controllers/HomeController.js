31e5f35adc1ec47940a8163a90d36113859d4270
Andy Barratt <andy.barratt@ncr.com>
2015-12-23 13:55:28 +0000

    Added weather icons.

--- web/app/controllers/HomeController.js
+++ web/app/controllers/HomeController.js
@@ -34,4 +34,47 @@ angular.module('openboiler').controller('HomeController', ['$scope', '$filter', 
     $scope.stopOverride = function () {
         $scope.isManual = false;
     }
+
+    $scope.getWeatherFontClass = function () {
+	switch($scope.weatherData.icon) {
+	    case "01d":
+		return "wi wi-day-sunny";
+	    case "02d":
+		return "wi wi-day-cloudy";
+	    case "03d":
+		return "wi wi-cloud";
+	    case "04d":
+		return "wi wi-cloudy";
+	    case "09d":
+		return "wi wi-showers";
+	    case "10d":
+		return "wi wi-day-rain";
+	    case "11d":
+		return "wi wi-day-lightning";
+	    case "13d":
+		return "wi wi-day-snow";
+	    case "50d":
+		return "wi wi-fog";
+	    case "01n":
+		return "wi wi-night-clear";
+	    case "02n":
+		return "wi wi-night-cloudy";
+	    case "03n":
+		return "wi wi-cloud";
+	    case "04n":
+		return "wi wi-cloudy";
+	    case "09n":
+		return "wi wi-showers";
+	    case "10n":
+		return "wi wi-night-rain";
+	    case "11n":
+		return "wi wi-night-lightning";
+	    case "13n":
+		return "wi wi-night-snow";
+	    case "50n":
+		return "wi wi-fog";
+	    default:
+		return "wi wi-cloud-refresh";
+        }
+    }
 }]);
